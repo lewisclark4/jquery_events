0b5f17d5abe24f5c08abff5c0686836e988d8c3a
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-01-15 14:24:14 +0000

    added various jquery functions to interact with .css

--- 03-jQueryEvents/02-events_in_jquery/challenge_solution/script.js
+++ 03-jQueryEvents/02-events_in_jquery/challenge_solution/script.js
@@ -1,82 +1,65 @@
 //waits until page is loaded first
 $(document).ready(function(){
 
-    //applies colour red to paragraphs when clicked on 
+    //applies colour red to paragraphs on click
+ 
     $("p").click(function(){
         $("p").css('color', 'red');
     });
 
-    //will add lightblue to h2 elements
+    //applies color lightblue to h2 elements on hover
+  
     $("h2").hover(function(){
-        $("h2").css('background-color', 'lightblue'); 
+        $("h2").css('background-color', 'lightblue');
     });
-
-    /*
-    this will apply larger font size to the active h2 element 
-    by adding the h2_font_size class but 
-    not the other h2 elements by removing class h2_font_size from them
+    /* Increases font size of h2 to 1.5em on hover and returns to 1em when no longer on hover
     */
+
     $("#hr_html").hover(function(){
-        $("#hr_mysql").css('font-size', '1em');
-        $("#hr_python").css('font-size', '1em');
-        $("#hr_jquery").css('font-size', '1em');
-        $("#hr_django").css('font-size', '1em');
-        $("#hr_css").css('font-size', '1em');
-        $("#hr_html").css('font-size', '2em');
+        $("#hr_html").css('font-size', '1.5em');
+    }, function(){
+        $("#hr_html").css('font-size', '1em');
     });
 
     $("#hr_mysql").hover(function(){
-        $("#hr_mysql").css('font-size', '2em');
-        $("#hr_python").css('font-size', '1em');
-        $("#hr_jquery").css('font-size', '1em');
-        $("#hr_django").css('font-size', '1em');
-        $("#hr_css").css('font-size', '1em');
-        $("#hr_html").css('font-size', '1em');
+        $("#hr_mysql").css('font-size', '1.5em');
+    }, function(){
+        $("#hr_mysql").css('font-size', '1em');
     });
 
     $("#hr_python").hover(function(){
-        $("#hr_mysql").css('font-size', '1em');
-        $("#hr_python").css('font-size', '2em');
-        $("#hr_jquery").css('font-size', '1em');
-        $("#hr_django").css('font-size', '1em');
-        $("#hr_css").css('font-size', '1em');
-        $("#hr_html").css('font-size', '1em');
+        $("#hr_python").css('font-size', '1.5em');
+    }, function(){
+        $("#hr_python").css('font-size', '1em');
     });
 
     $("#hr_jquery").hover(function(){
-        $("#hr_mysql").css('font-size', '1em');
-        $("#hr_python").css('font-size', '1em');
-        $("#hr_jquery").css('font-size', '2em');
-        $("#hr_django").css('font-size', '1em');
-        $("#hr_css").css('font-size', '1em');
-        $("#hr_html").css('font-size', '1em');
+        $("#hr_jquery").css('font-size', '1.5em');
+    }, function(){
+        $("#hr_jquery").css('font-size', '1em');
     });
 
     $("#hr_django").hover(function(){
-        $("#hr_mysql").css('font-size', '1em');
-        $("#hr_python").css('font-size', '1em');
-        $("#hr_jquery").css('font-size', '1em');
-        $("#hr_django").css('font-size', '2em');
-        $("#hr_css").css('font-size', '1em');
-        $("#hr_html").css('font-size', '1em');
-    });
-
-    $("#hr_css").hover(function(){
-        $("#hr_mysql").css('font-size', '1em');
-        $("#hr_python").css('font-size', '1em');
-        $("#hr_jquery").css('font-size', '1em');
+        $("#hr_django").css('font-size', '1.5em');
+    }, function(){
         $("#hr_django").css('font-size', '1em');
-        $("#hr_css").css('font-size', '2em');
-        $("#hr_html").css('font-size', '1em');
     });
 
-    //applies colour black to body background when mouse enters over buttons
-    $(".bottom_button").mouseenter(function(){
-        $("body").css( "background-color", "black"); 
+       $("#hr_css").hover(function(){
+        $("#hr_css").css('font-size', '1.5em');
+    }, function(){
+        $("#hr_css").css('font-size', '1em');
     });
 
-    //applies colour grey to body background when mouse leaves buttons
-    $(".bottom_button").mouseleave(function(){
-        $("body").css( "background-color", "#eee"); 
+
+    /*applies colour black to body background when mouse enters over buttons 
+    and applies colour grey to body background when mouse leaves buttons*/
+   
+    $(".bottom_button").hover(function(){
+        $("body").css("background-color", "black");
+    }, function(){
+        $("body").css("background-color", 'grey');
     });
+    //
+  
 });
